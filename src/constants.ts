b68c590d2f53e48bd1f7e0ca7d0e8ea9d32c1eaf
benaor <benjamingirard25@gmail.com>
2021-12-15 16:44:28 +0100

    Constant color

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,11 @@
 export enum Colors {
   WHITE = '#ffffff',
+  TRANSPARENT = 'transparent',
   LIGHT_GRAY = '#484B5B',
   LIGHTER_GRAY = '#73767F',
+  GRAY = '#5D6972',
   DARK_GRAY = '#595C66',
   DARKER_GRAY = '#2C2D35',
-  GRAY = '#5D6972',
+  ERROR = '#B00020',
+  BLUE = '#496ABF',
 }
