b75547aceb285ab0997c9ffd151325b63fcc30b2
benaor <benjamingirard25@gmail.com>
2021-12-16 10:57:57 +0100

    App, config and some tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['./src/setupTests.ts'],
+  setupFilesAfterEnv: [
+    './src/setupTests.ts',
+    '@testing-library/jest-dom/extend-expect',
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@react-native|react-native|@react-native-community|@react-navigation)',
   ],
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,15 +3,40 @@ import React from 'react'
 import {View} from 'react-native'
 import App from '../App'
 import AppNavigator from '../screens'
+import { Provider } from 'react-redux'
+import store from '../store'
 
 jest.mock('../screens', () => jest.fn())
-
+jest.mock('react-redux', () => {
+  return {
+    ...jest.requireActual<object>('react-redux'),
+    Provider: jest.fn(),
+  }
+})
 describe('App', () => {
+
+  
   test('should render routes', () => {
-    ;(AppNavigator as jest.Mock).mockReturnValueOnce(
+
+    (Provider as jest.Mock).mockImplementationOnce(({children}) => children)
+
+    (AppNavigator as jest.Mock).mockReturnValueOnce(
       <View testID="mock-routes" />,
     )
     const wrapper = render(<App />)
     wrapper.getByTestId('mock-routes')
+  })
+
+  test('should render provider', () => {
+    let providerStore!: typeof store 
+    (Provider as jest.Mock).mockImplementationOnce(({store}) => {
+    providerStore = store
+    return <View testID="mock-provider" />
+  })
+
+    const wrapper =  render(<App />) 
+    wrapper.getByTestId('mock-provider')
+
+    expect(providerStore).toBe(store)
   })
 })
